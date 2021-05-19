6796925ca5935ed51a610f26de52f1507e4c70ab
Maezzzzz <63753455+Maezzzzz@users.noreply.github.com>
2021-05-19 17:07:01 -0400

    Add files via upload

--- Contacts.java
+++ Contacts.java
@@ -1,70 +1,100 @@
 import java.util.*;
+import java.io.*;
 
 public class Contacts {
     static ArrayList<String> contacts = new ArrayList<String>();
-
+    static Scanner keyboard = new Scanner(System.in);
+    int option = 0;
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
 
-        populateContacts();
+        instructions();
 
-        System.out.print("Enter the contacts name to see if they are saved: ");
-        String name = keyboard.next().toLowerCase();
+        start();
 
-        searchSequential(contacts, name);
-        searchBinary(contacts, name);
+        save();
 
         //System.out.println(contacts);
     }
 
-    private static void populateContacts(){
-        contacts.add("Jane");
-        contacts.add("Joe");
-        contacts.add("Jack");
-        contacts.add("Jose");
-        contacts.add("Fred");
-        contacts.add("Albert");
-        contacts.add("Haily");
-        contacts.add("Amanda");
-        contacts.add("Thomas");
-        contacts.add("Kevin");
-        contacts.add("Timmy");
-        contacts.add("Charlie");
-        contacts.add("Devin");
-        contacts.add("Eugene");
-        contacts.add("Sam");
+    private static void instructions() {
+        System.out.println("Welcome to the contact application!");
+        System.out.println("The system allows you to add, store, search and delete contacts.");
+
+    }
+
+    private static void start(int option){
+        while (option != 4) {
+            System.out.println("Enter '0' to import contacts");
+            System.out.println("Enter '1' to add a contact.");
+            System.out.println("Enter '2' to delete a contact.");
+            System.out.println("Enter '3' to search for a contact.");
+            System.out.println("Enter '4' to save and quit.");
+
+            if(keyboard.nextInt() == 0) {
+                importContacts();
+            }
+            if(keyboard.nextInt() == 1) {
+                //TODO
+            }
+            if(keyboard.nextInt() == 2) {
+                //TODO
+            }
+            if(keyboard.nextInt() == 3) {
+                //TODO
+            }
+            if(keyboard.nextInt() == 4) {
+                //TODO
+            }
+        }
+        save();
+    }
+
+    private static void importContacts() {
+        //TODO
+    }
+
+    private static void addContact() {
+        System.out.print("Enter Contacts name: ");
+        contacts.add(keyboard.next());
+
+    }
+
+    private static void save() {
+        try {
+            FileWriter myWriter = new FileWriter("Contacts.txt");
+            for(int i = 0; i < contacts.size(); i++) {
+                myWriter.write(contacts.get(i) + "\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void sort(){
         Collections.sort(contacts);
     }
 
-    private static void searchSequential(ArrayList<String> list, String nameToSearch) {
-        for(int i = 0; i < list.size();i++) {
-            if(nameToSearch.equals(list.get(i).toLowerCase())) {
-                System.out.println(nameToSearch + " was found!");
-                return;
-            }
-        }
-        System.out.println(nameToSearch + " was not found.");
-        return;
-    }
-
-    private static void searchBinary(ArrayList<String> list, String nameToSearch) {
-        if(list.size() >= 1) {
+    private static void searchBinary(String nameToSearch) {
+        if(contacts.size() >= 1) {
             int left = 0;
-            int right = list.size() - 1;
+            int right = contacts.size() - 1;
 
             while(left <= right) {
                 int mid = left + (right-left) / 2;
 
-                if(nameToSearch.toLowerCase().equals(list.get(mid).toLowerCase())) {
+                if(nameToSearch.toLowerCase().equals(contacts.get(mid).toLowerCase())) {
                     System.out.println(nameToSearch + " was found!");
                     return;
                 }
 
-                if(nameToSearch.toLowerCase().compareTo(list.get(mid).toLowerCase()) > 0) {
+                if(nameToSearch.toLowerCase().compareTo(contacts.get(mid).toLowerCase()) > 0) {
                     left = mid + 1;
                 }
 
-                if(nameToSearch.toLowerCase().compareTo(list.get(mid).toLowerCase()) < 0) {
+                if(nameToSearch.toLowerCase().compareTo(contacts.get(mid).toLowerCase()) < 0) {
                     right = mid - 1;
                 }
             }
@@ -73,35 +103,4 @@ public class Contacts {
         }
     }
 
-    private static void searchJump(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchInterpolation(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchExponential(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchSublist(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchFibonacci(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchUbiquitousBinary(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchURecursion(ArrayList<String> list, String nameToSearch) {
-
-    }
-
-    private static void searchUnboundBinary(ArrayList<String> list, String nameToSearch) {
-
-    }
 }
